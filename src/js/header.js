135006c738d9888b984790642709868f221d27d9
YevhenKharchenko <evgenvalerych@gmail.com>
2025-04-06 22:51:44 +0300

    Update styles

--- src/js/header.js
+++ src/js/header.js
@@ -8,27 +8,35 @@ function onMenuButtonClick() {
   contextMenu.classList.add('is-open');
   menuBtn.classList.add('closed');
   closeContextBtn.classList.add('is-open');
-  // document.body.classList.add('screen-freeze');
 
   contextMenu.addEventListener('click', onContextMenuLinkClick);
   closeContextBtn.addEventListener('click', onCloseContextButtonClick);
+  document.addEventListener('click', onOutsideMenuClick);
 }
 
 function onCloseContextButtonClick() {
   contextMenu.classList.remove('is-open');
   closeContextBtn.classList.remove('is-open');
   menuBtn.classList.remove('closed');
-  // document.body.classList.remove('screen-freeze');
 
   contextMenu.removeEventListener('click', onContextMenuLinkClick);
   closeContextBtn.removeEventListener('click', onCloseContextButtonClick);
+  document.removeEventListener('click', onOutsideMenuClick);
 }
 
 function onContextMenuLinkClick(e) {
-  if (
-    e.target.nodeName === 'A' ||
-    !e.target.classList.contains('context-menu')
-  ) {
+  if (e.target.nodeName === 'A') {
     onCloseContextButtonClick();
+  }
+}
+
+function onOutsideMenuClick(e) {
+  const isClickInsideMenu = contextMenu.contains(e.target);
+  const isClickOnMenuBtn = menuBtn.contains(e.target);
+  const isClickOnCloseBtn = closeContextBtn.contains(e.target);
+
+  if (!isClickInsideMenu && !isClickOnMenuBtn && !isClickOnCloseBtn) {
+    onCloseContextButtonClick();
+    document.removeEventListener('click', onOutsideMenuClick);
   }
 }
